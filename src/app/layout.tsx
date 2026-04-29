268ae40b60ddfb751d84198630ecc56327156051
Vanessa <vanessalengkong5@gmail.com>
2026-04-29 15:35:30 +0800

    add shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, Inter } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/react";
 
 import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
 	title: "Tugas ETS",
@@ -19,7 +22,7 @@ export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html className={`${geist.variable}`} lang="en">
+		<html className={cn(geist.variable, "font-sans", inter.variable)} lang="en">
 			<body>
 				<TRPCReactProvider>{children}</TRPCReactProvider>
 			</body>
